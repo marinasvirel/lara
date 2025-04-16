cd8b78c710f1aa9fb6872b3ac04649bf328b89ba
marinasvirel <marusenka_83@mail.ru>
2025-04-16 07:14:05 +0300

    Заполнение таблиц через сидеры

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        DB::table('nousers')->insert([
+            'first_name' => 'name',
+            'sex'  => 'female',
+            'age'  => '41',
+            'password'  => '123',
+            'salary'  => '300000',
+            'second_name'  => 'surname',
+            'born'  => '1983-04-03',
         ]);
     }
 }
