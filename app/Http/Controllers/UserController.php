31faf7c38b48d1c0334191d6f59a6bc58ec8b259
marinasvirel <marusenka_83@mail.ru>
2025-04-17 09:24:07 +0300

    Вывод полученных записей в представлении

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB; // подключаем фасад DB
+
+class UserController extends Controller
+{
+  public function show()
+  {
+    $users = DB::table('users')->get();
+    return view('user.show', ['users' => $users]);
+  }
+}
---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+  <table>
+    <tr>
+      <th>Имя</th>
+      <th>Email</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+    </tr>
+    @foreach ($users as $user)
+    <tr>
+      <td>{{ $user->name }}</td>
+      <td>{{ $user->email }}</td>
+      <td>{{ $user->age }}</td>
+      <td>{{ $user->salary }}</td>
+    </tr>
+    @endforeach
+  </table>
+</x-layout>
