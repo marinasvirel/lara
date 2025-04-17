7f44a00db54f6173a9c4e3168e311f109495474a
marinasvirel <marusenka_83@mail.ru>
2025-04-17 10:05:15 +0300

    Сложные условия при выборке через QB

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,22 @@ class UserController extends Controller
 {
   public function show()
   {
+    // $users = DB::table('users')
+    //   ->where('salary', 500)
+    //   ->orWhere(function ($query) {
+    //     $query
+    //       ->where('age', '>=', 20)
+    //       ->where('age', '<=', 30);
+    //   })
+    //   ->get();
     $users = DB::table('users')
-    ->where('age', 30)
-    ->orWhere('id', '>', 4)
-    ->orWhere('salary', 500)
+    ->where('age', '<=', 30)
+    ->where('age', '>=', 20)
+    ->orWhere(function ($query) {
+      $query
+        ->where('salary', '>=', 800)
+        ->where('salary', '<=', 400);
+    })
     ->get();
     return view('user.show', ['users' => $users]);
   }
