739367e90e2266c560c1419ea754addb5ccb881a
marinasvirel <marusenka_83@mail.ru>
2025-05-07 09:29:57 +0300

    Условия в связи один ко многим

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -9,13 +9,11 @@ class CountryController extends Controller
 {
   public function show()
   {
-    $countries = Country::with('cities')->get();
-    // foreach ($countries as $country) {
-    //   echo "<h2>" . $country->name . "</h2>";
-    //   foreach ($country->cities as $key => $city) {
-    //     echo "<p>" . $city->name . "</p>";
-    //   }
-    // }
+    $countries = Country::with(['cities' => function ($query) {
+      $query->where('population', '>', 10000)
+      ->orderBy('population', 'asc');
+    }])
+      ->get();
     return view('country.show', ['countries' => $countries]);
   }
 }
--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -14,38 +14,47 @@ class CitySeeder extends Seeder
     DB::table('cities')->insert([
       [
         'name' => 'city11',
+        'population' => 10000,
         'country_id' => 1
       ],
       [
         'name' => 'city12',
+        'population' => 20000,
         'country_id' => 1
       ],
       [
         'name' => 'city13',
+        'population' => 20000,
         'country_id' => 1
       ],
       [
         'name' => 'city21',
+        'population' => 10000,
         'country_id' => 2
       ],
       [
         'name' => 'city22',
+        'population' => 20000,
         'country_id' => 2
       ],
       [
         'name' => 'city23',
+        'population' => 20000,
         'country_id' => 2
       ],
       [
         'name' => 'city31',
+        'population' => 10000,
         'country_id' => 3
       ],
       [
         'name' => 'city32',
+        'population' => 20000,
         'country_id' => 3
       ],
       [
         'name' => 'city33',
+        'population' => 20000,
         'country_id' => 3
       ],
     ]);
