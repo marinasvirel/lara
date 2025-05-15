238324cbcd09486a0e837b8c9502464c92b7731b
marinasvirel <marusenka_83@mail.ru>
2025-05-15 11:06:33 +0300

    Компоненты в макете сайта

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>{{ $title }}</title>
 </head>
 
 <body>
-  {{ $slot }}
+  <header></header>
+  <main>
+    {{ $slot }}
+  </main>
+  <aside class="aside-left"></aside>
+  <aside class="aside-right"></aside>
+  <footer></footer>
 </body>
 
 </html>
